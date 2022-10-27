bdc2845acc4b2b23e4acb35bb74f25f76ae23639
tunatuna tunatuna <113930372+Maverickstomcats@users.noreply.github.com>
2022-10-27 17:21:58 +0000

    hi

---
+++ main.py
@@ -0,0 +1,12 @@
+#from methods_help import method_help
+from functions import function, welcome,add_numbers,numdividedFrom_add_numbers
+#from returnStuff import returnS
+
+#method_help()
+welcome(" Alan Shera")
+added = add_numbers(4,8)
+print(added)
+div=numdividedFrom_add_numbers(5,added)
+print(div)
+function()
+# returnS()
---
+++ practice problems.py
@@ -0,0 +1,23 @@
+# go to week 10  to find Day 5 exercises and complete them here.(there are 4 exercises)
+#Create a function called return_distincts() that receives 3
+#integers as parameters.
+
+#If the sum of the 3 numbers is greater than 15, it must return
+#the highest number.
+
+#f the sum of the 3 numbers is less than 10, it must return the
+#lowest number.
+
+#If the sum of the 3 numbers is a value between 10 and 15
+#(included), then it must return the number with the
+#intermediate value.
+import random 
+
+def return_distincts():
+  numberone =random.randint(1,10)
+  numbertwo = random.randint(1,10)
+  numberthree = random.randint(1,10)
+  list = [numberone, numbertwo, numberthree]
+
+if list < 10 
+print
