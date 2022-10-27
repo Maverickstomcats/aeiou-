337e0c764a761431afab1b41b2a9557c13d029a2
tunatuna tunatuna <113930372+Maverickstomcats@users.noreply.github.com>
2022-10-27 17:52:30 +0000

    monihjmkfgidirhvgregdfgwettfrtternrth

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #from methods_help import method_help
-from functions import welcome,add_numbers,numdividedFrom_add_numbers,greet,square
-#from returnStuff import returnS
+#from functions import welcome,add_numbers,numdividedFrom_add_numbers,greet,square
+from returnStuff import returnS, powah, usd_to_euro
 
 #method_help()
-welcome(" Alan Shera")
-added = add_numbers(4,8)
-print(added)
-div=numdividedFrom_add_numbers(5,added)
-print(div)
-greet()
-fart = square(10)
-print(fart)
-# returnS()
+#welcome(" Alan Shera")
+#added = add_numbers(4,8)
+#print(added)
+#div=numdividedFrom_add_numbers(5,added)
+#print(div)
+#greet()
+#fart = square(10)
+#print(fart) 
+returnS()
+fartymcfart = powah(4,6)
+print(fartymcfart)
+moni = usd_to_euro(10)
+print(moni)
--- returnStuff.py
+++ returnStuff.py
@@ -1,14 +1,14 @@
 def returnS():
   pass
-
 ########################################################################################################################
     # allow us to create blocks of code that can be easily executed  many times and in different contexts without the need to rewrite the entire block
   # Return Practice #1
   # Create a function called power that takes two numeric values as arguments. It must return the number that results from solving a power, 
   # using the first number as the base, and the second as the exponent:
 
-
-
+def powah(num1, num2):
+  powahs = num1 ** num2
+  return powahs
 
 
 
@@ -20,8 +20,9 @@ def returnS():
 
 # Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
 
-
-
+def usd_to_euro(USD):
+  EUR = USD * 0.90
+  return EUR
 
 
 
